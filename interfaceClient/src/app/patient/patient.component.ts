0a5cf70b3c9df10be2005f394af7ae21d65886bb
Myohres <77498044+Myohres@users.noreply.github.com>
2023-03-31 05:44:30 +0200

    research patient interface

---
+++ interfaceClient/src/app/patient/patient.component.ts
@@ -0,0 +1,76 @@
+import { Component, OnInit } from '@angular/core';
+import { Patient } from '../model/patient';
+import { Observable } from 'rxjs';
+import {PatientService} from "../service/patient.service";
+import {FormControl} from "@angular/forms";
+
+@Component({
+  selector: 'app-patient',
+  templateUrl: './patient.component.html',
+  styleUrls: ['./patient.component.css']
+})
+export class PatientComponent implements OnInit {
+
+  public patientData: Patient[] = [];
+  public lastName = new FormControl();
+  public firstName = new FormControl();
+  public dob = new FormControl();
+  public sex = new FormControl();
+  public address = new FormControl();
+  public phoneNumber = new FormControl();
+
+  constructor(private patientService: PatientService) { }
+
+  ngOnInit(): void {
+    this.patientData = []
+    this.lastName.setValue("")
+    this.firstName.setValue("")
+    this.dob.setValue("")
+    this.sex.setValue("")
+    this.address.setValue("")
+    this.phoneNumber.setValue("")
+
+  }
+
+  getAllPatients(){
+    this.patientService.getAllPatients()
+      .subscribe({
+        next: data => this.patientData = data,
+        error: err => console.error(+err),
+        complete: () => console.log("get all patients complete")
+      })
+  }
+
+  getPatientsByPhone(){
+   this.patientData = [];
+    this.patientService.getPatientByPhone(this.phoneNumber.value)
+      .subscribe({
+        next: data => this.patientData = data,
+        error: err => console.error(+err),
+        complete: () => console.log("get patient by phone complete")
+      })
+  }
+
+  getPatient(){
+
+    this.patientService.getPatientAllInformation(this.firstName.value, this.lastName.value, this.dob.value, this.sex.value, this.address.value, this.phoneNumber.value)
+      .subscribe({
+        next: data => this.patientData = data,
+        error: err => console.error(+err),
+        complete: () => console.log("get patient complete")
+      })
+
+    this.lastName.reset()
+    this.firstName.reset()
+    this.dob.reset()
+    this.sex.reset()
+    this.address.reset()
+    this.phoneNumber.reset()
+    this.lastName.setValue("")
+    this.firstName.setValue("")
+    this.dob.setValue("")
+    this.sex.setValue("")
+    this.address.setValue("")
+    this.phoneNumber.setValue("")
+  }
+}
