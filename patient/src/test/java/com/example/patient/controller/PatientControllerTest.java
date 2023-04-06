80ae34bf0259f0f681a4b04826fe982dc05245a0
Myohres <77498044+Myohres@users.noreply.github.com>
2023-04-06 02:23:31 +0200

    test ok

---
+++ patient/src/test/java/com/example/patient/controller/PatientControllerTest.java
@@ -0,0 +1,73 @@
+package com.example.patient.controller;
+
+import com.example.patient.document.Patient;
+import com.example.patient.repository.PatientRepository;
+import com.example.patient.service.PatientService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.ArrayList;
+import java.util.NoSuchElementException;
+
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@ExtendWith(MockitoExtension.class)
+class PatientControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @MockBean
+    PatientService patientService;
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void getPatientByAllInformationFound() throws Exception {
+        when(patientService.getPatientByAllInformation("","","","","","")).thenReturn(new ArrayList<>());
+        mvc.perform(get("/patient/")).andExpect(status().isOk());
+    }
+
+    @Test
+    void getPatientByAllInformationNotFound() throws Exception {
+        when(patientService.getPatientByAllInformation(any(),any(),any(),any(),any(),any())).thenThrow(new NoSuchElementException());
+        mvc.perform(get("/patient/")).andExpect(status().isNotFound());
+    }
+
+    @Test
+    void addPatient() throws Exception {
+        when(patientService.savePatient(new Patient())).thenReturn(new Patient());
+        mvc.perform(post("/patient/add")
+                .contentType(MediaType.APPLICATION_JSON).content("{}"))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void addPatientNotFound() throws Exception {
+        when(patientService.savePatient(any())).thenThrow(new NoSuchElementException());
+        mvc.perform(post("/patient/add")
+                .contentType(MediaType.APPLICATION_JSON).content("{}"))
+                .andExpect(status().isNotFound());
+    }
+
+
+}
---
+++ patient/src/test/java/com/example/patient/service/PatientServiceTest.java
@@ -0,0 +1,103 @@
+package com.example.patient.service;
+
+import com.example.patient.document.Patient;
+import com.example.patient.repository.PatientRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.NoSuchElementException;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class PatientServiceTest {
+
+    Patient patient1;
+    Patient patient2;
+    Patient patient3;
+    Patient patient4;
+
+    List<Patient> patientList;
+
+    @Mock
+    private PatientRepository patientRepository;
+
+    @InjectMocks
+    private PatientService patientService;
+
+    @BeforeEach
+    public void setUp() {
+        patientList = new ArrayList<Patient>();
+
+        patient1 = new Patient();
+        patient1.setId("1");
+        patient1.setFirstName("John");
+        patient1.setLastName("Connor");
+        patient1.setDob("18-04-1989");
+        patient1.setAddress("1 rue ville");
+        patient1.setSex("H");
+        patient1.setPhone("1234");
+        patientList.add(patient1);
+
+        patient2 = new Patient();
+        patient2.setId("2");
+        patient2.setFirstName("Jane");
+        patient2.setLastName("Seymour");
+        patient2.setDob("20-02-1970");
+        patient2.setAddress("3 av ville");
+        patient2.setSex("F");
+        patient2.setPhone("5678");
+        patientList.add(patient2);
+
+        patient3 = new Patient();
+        patient3.setId("3");
+        patient3.setFirstName("Will");
+        patient3.setLastName("Turner");
+        patient3.setDob("10-03-1985");
+        patient3.setAddress("10 rue Fort");
+        patient3.setSex("H");
+        patient3.setPhone("0000");
+        patientList.add(patient3);
+    }
+
+    @Test
+    void getPatientByAllInformationWithNoInfo() {
+       List<Patient> patientListTest = patientService.getPatientByAllInformation("","","","","","");
+       assertEquals(0, patientListTest.size());
+    }
+
+    @Test
+    void getPatientByAllInformationWithAllInfo() {
+        when(patientRepository.findAll()).thenReturn(patientList);
+        List<Patient> patientListTest = patientService.getPatientByAllInformation("John","Connor","18-04-1989","H","1 rue ville","1234");
+        assertEquals(1, patientListTest.size());
+    }
+
+    @Test
+    void getPatientByAllInformationWithSomeInformation() {
+        when(patientRepository.findAll()).thenReturn(patientList);
+        List<Patient> patientListTest = patientService.getPatientByAllInformation("","","","H","","");
+        assertEquals(2, patientListTest.size());
+    }
+
+    @Test
+    void getPatientByAllInformationWithSomeInformationAndNotFound() {
+        when(patientRepository.findAll()).thenReturn(patientList);
+       assertThrows(NoSuchElementException.class, () -> patientService.getPatientByAllInformation("","","","","","9999"));
+    }
+
+    @Test
+    void savePatient() {
+        when(patientRepository.save(any())).thenReturn(patient1);
+        patientService.savePatient(patient1);
+        verify(patientRepository,times(1)).save(patient1);
+    }
+}
